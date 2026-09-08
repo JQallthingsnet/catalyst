bcf6d0fdd781e24cf74c444311d6b86e0f5bdabe
Jerry Qiu <jerry_qiu@hotmail.com>
2026-09-08 20:36:12 +1000

    To update the SMTP setting to microsoft365 emails

--- lib/env.ts
+++ lib/env.ts
@@ -4,7 +4,7 @@ export type AppEnv = {
   DB?: D1Database;
   AUTH_SECRET?: string;
   AUTH_DEV_RETURN_CODE?: string;
-  "gmail-smtp-keys"?: string;
+  "smtp-keys"?: string;
 };
 
 export function getEnv(): AppEnv {
